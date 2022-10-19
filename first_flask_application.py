a5c18448951369a92edb24182947a11a6332e800
Lakshman Enlume <lakshman.p@enlume.com>
2022-10-19 17:45:54 +0530

    small fix in return statement

--- first_flask_application.py
+++ first_flask_application.py
@@ -5,7 +5,7 @@ app = Flask(__name__)
 
 @app.route("/")
 def hello_world():
-    return "Hello, World!, this is very nice way to write the code"
+    return "Hello, World!, this is very nice way to write the code."
 
 
 if __name__ == "__main__":
