ee208eca03e8ba501fa15827c7b51851843015dc
Lakshman Enlume <lakshman.p@enlume.com>
2022-10-28 15:56:14 +0530

    Add few more things in the return message

--- first_flask_application.py
+++ first_flask_application.py
@@ -5,7 +5,8 @@ app = Flask(__name__)
 
 @app.route("/")
 def hello_world():
-    return "Hello, World!, this is very nice way to write the code, and we wll definitely build something great."
+    return "Hello, World!, this is very nice way to write the code, and we wll definitely build something great." \
+           "We are building the application which we can used in future."
 
 
 if __name__ == "__main__":
